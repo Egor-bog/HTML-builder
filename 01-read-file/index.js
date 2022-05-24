3e0a56d44ee4ff8c59640266072e56c3dc8326f6
Egor <7548494@gmail.com>
2022-05-24 19:41:53 +0200

    add: 1 streem

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,21 +1,10 @@
-const { rejects } = require('assert');
-const fs = require ('fs');
-const { resolve } = require('path');
-const path = require ('path');
+const fs = require('fs');
+const path = require('path');
 
-const dirText = path.join(__dirname, 'text.txt')
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
 
-const readFileAsync = async (path) => {
-    return new Promise ((resolve, rejects) => fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
-        if (err) {
-            return rejects(err.message)
-        }
-        resolve(data)
-    }))
-}
+stream.pipe(process.stdout);
 
 
 
-readFileAsync(path.join(__dirname, 'text.txt'))
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -11,149 +11,72 @@ const template = fs.createReadStream(path.join(__dirname, 'template.html'));
 const componentsHtml = path.join(__dirname, 'components');
 let templateString = '';
 
-let newPath = path.join(__dirname, 'project-dist')
-
-let arrPath = [newFolder];
-
-let arrDirectorys = ['project-dist'];
 
 
 
-fs.mkdir(newFolder, {recursive: true}, err => {
+fs.mkdir(newFolder, {
+  recursive: true
+}, err => {
   if (err) throw err;
 });
 
-deleteFolder(newFolder)  
-
-function deleteFolder(pathF) { console.log(pathF)
-    fs.readdir(pathF, {withFileTypes: true}, function(err, files) {
-      if(err) throw err;
-        let dirArr = [];
-        files.forEach(file => {
-          if(file.isDirectory()) {
-            dirArr.push(file.name)
-          }
-          if(file.isFile()) {fs.unlink(path.join(pathF, `${file.name}`), err => {if(err) throw err;})}  
-        });
-        if(dirArr) deleteFolderTwo (dirArr, pathF)
-    })  
+async function copyDir(oldAssetss, newAssetss) {
+  await fs.promises.mkdir(newAssetss, {
+    recursive: true
+  }, (err) => {
+    if (err) throw err;
+  });
+  const files = await fs.promises.readdir(oldAssetss, {
+    withFileTypes: true
+  });
+  files.forEach(async (file) => {
+    if (file.isFile()) {
+      const oldFile = path.join(oldAssetss, file.name);
+      const newFile = path.join(newAssetss, file.name);
+      await fs.promises.copyFile(oldFile, newFile);
+    } else {
+      copyDir(path.join(oldAssetss, file.name), path.join(newAssetss, file.name));
+    }
+  });
 }
+copyDir(oldAssetss, newAssetss);
 
-function deleteFolderTwo(dirArr, pathF) {
-  let newDirArr = []
-  for ( let i = 0; i < dirArr.length; i++) {
-    fs.readdir(path.join(pathF, dirArr[i]), {withFileTypes: true}, function(err, files) { 
-      {if(err) throw err;}
-      if(files.length == 0) {
-        fs.rmdir(path.join(pathF, dirArr[i]), err => {if(err) throw err;})
-        console.log(path.join(pathF, '..'))
-        deleteFolderTwo(dirArr, path.join(pathF, '..'))
-      }
-      files.forEach(file => {
-        if(file.isDirectory()) {
-          newDirArr.push(file.name); 
-        }  
-        if(file.isFile()) {fs.unlink(path.join(pathF, dirArr[i], file.name), err => {if(err) throw err;})} 
-      })
-      if(newDirArr.length > 0) {
-        deleteFolderTwo( newDirArr , path.join(pathF, dirArr[i])); 
-      } else if(newDirArr.length = 0) { deleteFolderTwo(dirArr, pathF)}
-    })
+fs.readdir(stylesPath, (err, files) => {
+  const cssFile = fs.createWriteStream(path.join(newFolder, 'style.css'));
+  if (err) throw err;
+  for (let i = 0; i < files.length; i++) {
+    let extension = path.extname(files[i]).split('.').pop();
+    if (extension === 'css') {
+      const input = fs.createReadStream(path.join(stylesPath, files[i]));
+      input.on('data', data => {
+        cssFile.write(data.toString() + '\n');
+      });
+    }
   }
-}  
+});
 
-
-
-
-  // for ( let i = 0; i < files.length; i++) { 
-  //   console.log(path.join(__dirname, files[i]).name)
-  //   let pathFile = path.join(__dirname, files[i])
-  //   fs.stat(pathFile, (err, file) => {
-  //     if (err) {
-  //       console.error(err)
-  //       return
-  //     }
-
-  //   })  
-  //   if(file.isDirectory()) {
-  //     delFils(pathFile)
-  //   } else {
-  //     fs.unlink(pathFile, err => {
-  //       if(err) throw err
-  //     })
-  //   } 
-  // }
-    //files.forEach(file => fs.unlink(path.join(__dirname, file)))
-
-
-
-
-
-//console.log(map)  
-
-// fs.mkdir(newFolder, {
-//   recursive: true
-// }, err => {
-//   if (err) throw err;
-// });
-
-// async function copyDir(oldAssetss, newAssetss) {
-//   await fs.promises.mkdir(newAssetss, {
-//     recursive: true
-//   }, (err) => {
-//     if (err) throw err;
-//   });
-//   const files = await fs.promises.readdir(oldAssetss, {
-//     withFileTypes: true
-//   });
-//   files.forEach(async (file) => {
-//     if (file.isFile()) {
-//       const oldFile = path.join(oldAssetss, file.name);
-//       const newFile = path.join(newAssetss, file.name);
-//       await fs.promises.copyFile(oldFile, newFile);
-//     } else {
-//       copyDir(path.join(oldAssetss, file.name), path.join(newAssetss, file.name));
-//     }
-//   });
-// }
-// copyDir(oldAssetss, newAssetss);
-
-// fs.readdir(stylesPath, (err, files) => {
-//   const cssFile = fs.createWriteStream(path.join(newFolder, 'style.css'));
-//   if (err) throw err;
-//   for (let i = 0; i < files.length; i++) {
-//     let extension = path.extname(files[i]).split('.').pop();
-//     if (extension === 'css') {
-//       const input = fs.createReadStream(path.join(stylesPath, files[i]));
-//       input.on('data', data => {
-//         cssFile.write(data.toString() + '\n');
-//       });
-//     }
-//   }
-// });
-
-// async function creteHtmlPage() {
-//   const htmlFile = fs.createWriteStream(path.join(newFolder, 'index.html'));
-//   template.on('data', data => {
-//     templateString = data.toString();
-//     fs.readdir(componentsHtml, {
-//       withFileTypes: true
-//     }, (err, files) => {
-//       if (err) throw err;
-//       files.forEach((item, i) => {
-//         if (item.isFile() && path.parse(item.name).ext === '.html') {
-//           const readComp = fs.createReadStream(path.join(__dirname, 'components', item.name));
-//           const nameComp = path.parse(item.name).name;
-//           const extens = `{{${nameComp}}}`;
-//           readComp.on('data', data => {
-//             templateString = templateString.replace(extens, data.toString());
-//             if (i === files.length - 1) {
-//               htmlFile.write(templateString);
-//             }
-//           });
-//         }
-//       });
-//     });
-//   });
-// }
-// creteHtmlPage();
+async function creteHtmlPage() {
+  const htmlFile = fs.createWriteStream(path.join(newFolder, 'index.html')); 
+  template.on('data', data => {
+    templateString = data.toString();
+    fs.readdir(componentsHtml, {
+      withFileTypes: true
+    }, (err, files) => {
+      if (err) throw err;
+      files.forEach((item, i) => {
+        if (item.isFile() && path.parse(item.name).ext === '.html') {
+          const readComp = fs.createReadStream(path.join(__dirname, 'components', item.name));
+          const nameComp = path.parse(item.name).name;
+          const extens = `{{${nameComp}}}`;
+          readComp.on('data', data => {
+            templateString = templateString.replace(extens, data.toString());
+            if (i === files.length - 1) {
+              htmlFile.write(templateString);
+            }
+          });
+        }
+      });
+    });
+  });
+}
+creteHtmlPage();
